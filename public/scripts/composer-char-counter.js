8b4bc7010951ecd0de5cf9593bd8264eb29d992e
Justin <justinanderson101@gmail.com>
2021-03-23 23:26:05 +0000

    implementing character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  console.log('The document is ready!')
+  $('#tweet-text').on('input', function(event) {
+    const totalCharacters = $(this).val().length;
+    let charactersLeft = 140 - totalCharacters;
+    const counter = $(this).parent().next('div').children('.counter')
+    counter.text(charactersLeft)
+    if (charactersLeft <= 0) {
+      counter.addClass('red-alert')
+    } else {
+      counter.removeClass('red-alert')
+    }
+  });
+});
