fd8fbacf4c30d6bb79f2713221b7c0f3c4f54360
Justin <justinanderson101@gmail.com>
2021-03-24 02:28:00 +0000

    implementing tweet container

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
-  console.log('The document is ready!')
+  console.log('The document is ready!');
   $('#tweet-text').on('input', function(event) {
     const totalCharacters = $(this).val().length;
     let charactersLeft = 140 - totalCharacters;
-    const counter = $(this).parent().next('div').children('.counter')
-    counter.text(charactersLeft)
+    const counter = $(this).parent().next('div').children('.counter');
+    counter.text(charactersLeft);
     if (charactersLeft <= 0) {
-      counter.addClass('red-alert')
+      counter.addClass('red-alert');
     } else {
-      counter.removeClass('red-alert')
+      counter.removeClass('red-alert');
     }
   });
 });
