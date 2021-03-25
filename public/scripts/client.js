d2ed55754f2a7a5262fe73530bc1b7c7c24e2f8c
Justin <justinanderson101@gmail.com>
2021-03-25 22:13:45 +0000

    fixing form submission with AJAX

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,40 +1,6 @@
 $(document).ready(function () {
-const data = [
-  // {
-  //   "user": {
-  //     "name": "Robert Hunter",
-  //     "avatars": "https://i.imgur.com/73hZDYK.png"
-  //     ,
-  //     "handle": "@TheRealHunter1965"
-  //   },
-  //   "content": {
-  //     "text": "Without love in the dream, it'll never come true..."
-  //   },
-  //   "created_at": 1461784933487
-  // },
-  // {
-  //   "user": {
-  //     "name": "Newton",
-  //     "avatars": "https://i.imgur.com/73hZDYK.png"
-  //     ,
-  //     "handle": "@SirIsaac"
-  //   },
-  //   "content": {
-  //     "text": "If I have seen further it is by standing on the shoulders of giants"
-  //   },
-  //   "created_at": 1461116232227
-  // },
-  // {
-  //   "user": {
-  //     "name": "Descartes",
-  //     "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //     "handle": "@rd" },
-  //   "content": {
-  //     "text": "Je pense , donc je suis"
-  //   },
-  //   "created_at": 1461113959088
-  // }
-]
+
+const data = [];
 
 const timeSinceTweet = (unix) => {
   return moment(unix).fromNow();
